665980c32a6d074f243b4344f3b699664acb259e
Rick C Metcalf <rick.c.metcalf@gmail.com>
2022-03-07 13:59:40 -0500

    Fix gitignore

--- GardenPlanner/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ GardenPlanner/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace GardenPlanner {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GardenPlanner;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GardenPlanner;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
